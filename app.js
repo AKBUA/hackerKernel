c6b0fe81ad85c15c797029f0896d5747b3a9a5c5
Abhishek kushwaha <96886598+AKBUA@users.noreply.github.com>
2023-02-01 23:43:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const mongoose=require('mongoose')
+const userRoute=require('./routes/user');
+const taskRoute=require('./routes/task');
+const exportRoute=require('./routes/export');
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(userRoute)
+app.use(taskRoute)
+app.use(exportRoute)
+app.use(express.static("public"));
+mongoose.connect('mongodb://localhost:27017/hackerkernel',{
+  useNewUrlParser: true,
+   useUnifiedTopology: true
+},()=>{
+  console.log('db connected')
+})
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/user", (req, res) => {
+    res.render("user");
+  })
+  app.get("/task", (req, res) => {
+    res.render("task");
+  })
+app.get("/exportexcel", (req, res) => {
+    res.render("export");
+  });
+app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    })
